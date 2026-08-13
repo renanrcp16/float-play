780dd03403796509a5f8b8ffbdfead39dd1f0389
Renan Corrêa Pedroso <renan.rcp16@gmail.com>
2026-08-13 19:41:47 -0300

    feat: add fit control label

--- src/content.ts
+++ src/content.ts
@@ -8,17 +8,13 @@ const logger = new ConsoleLogger(false);
 const i18n = new ChromeI18n();
 const youtube = new YouTubeAdapter();
 const pip = new DocumentPipManager(logger);
-const controller = new FloatPlayController(
-  youtube,
-  pip,
-  {
-    play: i18n.getMessage("playAction", "Play"),
-    pause: i18n.getMessage("pauseAction", "Pause"),
-    backward: i18n.getMessage("backwardAction", "Skip backward 10 seconds"),
-    forward: i18n.getMessage("forwardAction", "Skip forward 10 seconds"),
-    timeline: i18n.getMessage("timelineAction", "Playback timeline")
-  },
-  logger
-);
+const controller = new FloatPlayController(youtube, pip, {
+  play: i18n.getMessage("playAction", "Play"),
+  pause: i18n.getMessage("pauseAction", "Pause"),
+  backward: i18n.getMessage("backwardAction", "Skip backward 10 seconds"),
+  forward: i18n.getMessage("forwardAction", "Skip forward 10 seconds"),
+  timeline: i18n.getMessage("timelineAction", "Playback timeline"),
+  fit: i18n.getMessage("fitAction", "Fit to video")
+}, logger);
 
 controller.start();
