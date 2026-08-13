a2ed516be44ab4f3e4083de32fdd3cabf9279a56
Renan Corrêa Pedroso <renan.rcp16@gmail.com>
2026-08-13 20:10:07 -0300

    chore: remove unused style module

--- src/presentation/player/PlayerStyles.ts
+++
@@ -1,9 +0,0 @@
-import { PLAYER_BASE_STYLES } from "./styles/PlayerBaseStyles";
-import { PLAYER_CONTROL_STYLES } from "./styles/PlayerControlStyles";
-
-export function installPlayerShellStyles(document: Document): void {
-  const style = document.createElement("style");
-  style.dataset.floatplay = "player-shell-styles";
-  style.textContent = `${PLAYER_BASE_STYLES}${PLAYER_CONTROL_STYLES}`;
-  document.head.append(style);
-}
