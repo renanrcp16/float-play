964b8bdf62d707c9c53c7c00e3fd6de619167e9f
Renan Corrêa Pedroso <renan.rcp16@gmail.com>
2026-08-13 19:41:20 -0300

    test: restore aspect-correct PiP sizing cases

--- src/infrastructure/pip/PipWindowSize.test.ts
+++ src/infrastructure/pip/PipWindowSize.test.ts
@@ -2,11 +2,11 @@ import { expect as assert, test as check } from "vitest";
 import { calculateInitialPipSize } from "./PipWindowSize";
 
 check("landscape sizing", () => {
-  assert(calculateInitialPipSize(1920, 1080)).toEqual({ width: 512, height: 288 });
+  assert(calculateInitialPipSize(1920, 1080)).toEqual({ width: 480, height: 270 });
 });
 
 check("4:3 landscape sizing", () => {
-  assert(calculateInitialPipSize(1440, 1080)).toEqual({ width: 512, height: 384 });
+  assert(calculateInitialPipSize(1440, 1080)).toEqual({ width: 480, height: 360 });
 });
 
 check("vertical sizing", () => {
@@ -14,5 +14,5 @@ check("vertical sizing", () => {
 });
 
 check("fallback sizing", () => {
-  assert(calculateInitialPipSize(0, 0)).toEqual({ width: 512, height: 288 });
+  assert(calculateInitialPipSize(0, 0)).toEqual({ width: 480, height: 270 });
 });
