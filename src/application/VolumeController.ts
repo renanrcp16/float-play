4a470b48e539d5a0fc5598a501fc17554e1ade5c
Renan Corrêa Pedroso <renan.rcp16@gmail.com>
2026-08-13 20:55:13 -0300

    refactor: narrow volume port

--- src/application/VolumeController.ts
+++ src/application/VolumeController.ts
@@ -1,11 +1,4 @@
-export interface VolumeState {
-  readonly volume: number;
-  readonly muted: boolean;
-}
-
-export interface VolumeController {
-  getState(): VolumeState;
+export interface VolumeMirror {
   setVolume(volume: number): void;
   setMuted(muted: boolean): void;
-  subscribe(listener: () => void, signal: AbortSignal): void;
 }
